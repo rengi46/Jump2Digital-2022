85cf17c6bf763e6937d9c9d3913a52193cc6193d
rogerpuig <rogerpuigdemasa@gmail.com>
2022-11-10 13:43:23 +0100

    Tests endpoints

--- tests/companys.test.js
+++ tests/companys.test.js
@@ -3,46 +3,62 @@ const { default: mongoose } = require('mongoose')
 const {app,server} = require("../index")
 
 const api = supertest(app)
-
-describe("company list",()=>{
-    test("endPoint Size is responded", ()=>{
-    api
-        .get("/api/v1/companies/size")
-        .expect(200)
-        .expect('content-type', /application\/json/ )
+describe("endPoints",()=>{
+    describe("endPoint Size",()=>{
+        test("is responded", ()=>{
+        api
+            .get("/api/v1/companies/size")
+            .expect(200)
+            .expect('content-type', /application\/json/ )
+        })
+        test("sort is correct", async()=>{
+            const response =  await api.get("/api/v1/companies/size")
+            const data = response.body.body
+            expect(Array.isArray(data)).toBe(true)
+            const mapData = data.map((company)=> {
+                let sizeNum = null
+                if(company.size !==null){
+                    sizeNum =Number(company.size.split("-")[0])
+                }
+                return {sizeMin:sizeNum, ...company }
+            })
+            expect(isSorted(mapData,"sizeMin")).toBe(true)
+        })
     })
-    test("sort is correct", async()=>{
-    const response =  await api.get("/api/v1/companies/size")
-    const data = response.body.body
-    expect(Array.isArray(data)).toBe(true)
-    const mapData = data.map((company)=> {
-        let sizeNum = null
-        if(company.size !==null){
-            sizeNum =Number(company.size.split("-")[0])
-        }
-        return {size:sizeNum, ...company }
+    describe("endPoint foundation",()=>{
+        test(" is responded", ()=>{
+            api
+                .get("/api/v1/companies/foundation")
+                .expect(200)
+                .expect('content-type', /application\/json/ )
+        })
+        test("is sorted",async()=>{
+            const response =  await api.get("/api/v1/companies/foundation")
+            const data = response.body.body
+            expect(Array.isArray(data)).toBe(true)
+            expect(isSorted(data,"founded")).toBe(true)
+        })
     })
-
-    expect(isSorted(mapData,"sizeMin")).toBe(true)
-
-    })
-    test("endPoint foundation is responded", ()=>{
-    api
-        .get("/api/v1/companies/foundation")
-        .expect(200)
-        .expect('content-type', /application\/json/ )
-    })
-    test("endPoint info is responded", ()=>{
-    api
-        .get("/api/v1/companies/info")
-        .expect(200)
-        .expect('content-type', /application\/json/ )
+    describe("endPoint info",()=>{
+        test("is responded", ()=>{
+            api
+                .get("/api/v1/companies/info")
+                .expect(200)
+                .expect('content-type', /application\/json/ )
+        })
+        test("info is correct", async()=>{
+                const response =  await api.get("/api/v1/companies/info")
+                const data = response.body.body
+                expect(Array.isArray(data)).toBe(true)
+                expect(data).toHaveLength(3)
+    
+        })
     })
     afterAll(()=>{
         mongoose.connection.close()
         server.close()
     })
-  })
+})
 
 
 const isSorted = (array,prop) => {
