e550f76252aace6bd79ce71c5892d240223c560b
rogerpuig <rogerpuigdemasa@gmail.com>
2022-11-10 13:42:16 +0100

    README

---
+++ README.md
@@ -0,0 +1,57 @@
+# JUMP2DIGITAL 2022
+## Background
+Proyecto creado con:
++ node.js
++ Express
++ mongoDB
+
+El proyecto sirve para gestionar una base de datos de compànyias
+## Usage
+
+Hacer peticion ajax o similar (postman), en el puerto http://localhost:5000/api/v1/companies
+
+## Installation
+
+
+dependencias
+```
+npm install
+```
+.env
+```
+Crea un archivo .env
+Crea una base de datos con mongo db
+Pega el archivo de companies.json situado en assets
+```
+
+iniciar proyecto
+```
+npm start
+```
+
+
+
+## See more
+El presente proyecto backend esta compuesto por tres endpoints:
+
++ /size:
+
+    devuelve la base de datos con las compañias ordenadas suegun su tamaño de empresa
+
++ /foundation:
+
+    devuelve la base de datos con las compañias ordenadas suegun su año de creacion
+
++ /info:
+
+    devuelve informacion de:
+        -cuantas empresas hay por cada tipo de empresas
+        -cuantas empresas hay por cada tamaño de empresa
+        -cuantas empresas hay por cada año de creacion
+
+## Contact info
+
+> rogerpuigdemasa@gmail.com || Roger Puigdemasa Armengol
+## License
+
+[Assembler Institute](https://assemblerinstitute.com)
--- controller/companysController.js
+++ controller/companysController.js
@@ -4,12 +4,15 @@ const {counByType} = require("../helpers/helpers")
 
 const getCompanysSortSize = async(req,res) => {
   try {
+    //take info for the DB
     const data = await companyModel.find({});
+    //sort the data with a size
     data.sort(function(a, b){
       const sizeA = Number(a.size.split("-")[1])
       const sizeB = Number(b.size.split("-")[1])
       return sizeA-sizeB
     })
+    //send data in the response
     response.success(req,res,data,200)
   }catch{
     response.error(req,res,"error al cojer informacion",400)
@@ -18,7 +21,9 @@ const getCompanysSortSize = async(req,res) => {
 
 const getCompanysSortFounded = async(req,res) => {
   try {
+    //take info for the DB
     const data = await companyModel.find({});
+    //sort the data with a size
     data.sort(function(a, b){
       if(Number(a.founded) == 0){
         return 1
@@ -28,6 +33,7 @@ const getCompanysSortFounded = async(req,res) => {
       }
       return Number(a.founded)-Number(b.founded)
     })
+    //send data in the response
     response.success(req,res,data,200)
   }catch{
     response.error(req,res,"error al cojer informacion",400)
@@ -37,11 +43,17 @@ const getCompanysSortFounded = async(req,res) => {
 
 const infoCompanies = async(req,res)=>{
   try {
+    //take info for the DB
     const data = await companyModel.find({});
+    // create temporal arra with saved all info
     const temporalArray = []
+    //save type of industrys
     temporalArray.push(counByType(data,"industry"))
+    //save type of size
     temporalArray.push(counByType(data,"size"))
+    //save type of founded
     temporalArray.push(counByType(data,"founded"))
+    //send data in the response
     response.success(req,res,temporalArray,200)
   } catch (error) {
     response.error(req,res,"error al cojer informacion",400)
